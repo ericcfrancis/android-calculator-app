ea57ba53d33ddfd5185a0645536426c8877aac18
ericcfrancis <ericcire49@gmail.com>
2023-11-13 01:35:17 +0800

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
   namespace = "com.eric.android_calculator_app"
-  compileSdk = 33
+  compileSdk = 34
 
   defaultConfig {
     applicationId = "com.eric.android_calculator_app"
